4189d9668aecbc36cbbed533f7fab6c5601369ac
besogogoladze <Gogoladzebeso8@gmail.com>
2022-02-24 16:15:23 +0100

    burger menu

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return <div className='position-relative'>
       <div className='bottom-0 p-xxl-5 w-100 bg-success d-flex justify-content-around align-items-baseline'>
-        <h1>parteners place 👇👇👇</h1>
+        <h1 className='text-light'>Footer 👇👇👇</h1>
       </div>
   </div>;
 }
--- src/Components/Header/BurgerMenu/BurgerMenu.js
+++ src/Components/Header/BurgerMenu/BurgerMenu.js
@@ -1,15 +1,13 @@
 import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import '../../index.css';
-import logo from '../../images/logo-tle.png'
+import { push as Menu } from 'react-burger-menu';
 import { useTranslation } from 'react-i18next';
-import './style.css'
-import BurgerMenu from './BurgerMenu/BurgerMenu';
+import { NavLink } from 'react-router-dom';
+import logo from '../../../images/logo-tle.png'
+import './style.css';
 
-function Header() {
+function BurgerMenu() {
 
-    const [language, setLanguage] = useState("fr");
-    const [isActive, setActive] = useState(false);
+  const [language, setLanguage] = useState("fr");
     
     const {t, i18n} = useTranslation();
 
@@ -19,32 +17,22 @@ function Header() {
         i18n.changeLanguage(lang);
         setLanguage(lang);
     }
-    function toggleClass() {
-        setActive(!isActive);
-        setTimeout(function(){
-            setActive(false);
-        }, 3000);
-    };
-    
-    
 
-  return <div>
-        <div className="burgerDisplay d-flex justify-content-end align-items-center m-2">
-            <BurgerMenu className="position-relative "/>
-        </div>
-        <main className="menuDisplay">
-            <header>
-                <div className="page">
-                    <nav className="page__menu menu">
-                        <ul className="menu__list r-list w-100  justify-content-around align-items-center">
+
+
+  return (
+        <Menu isOpen={false} width={ '50%' } right >
+            <div className=" h-100">
+                    <nav className="h-100 page__menu menu">
+                        <ul className="menu__list r-list w-100 h-100 flex-column justify-content-around align-items-center">
+                            <div className="align-center">
+                                <NavLink className="bg-transparent" exact to="/"><img className="fit-picture" src={logo} alt="Logo TLE" /></NavLink>
+                            </div>
                             <li className="menu__group"><NavLink exact to="/" className="menu__link r-link text-underlined">{t('Data.Header.1')}</NavLink></li>
                             <li className="menu__group"><NavLink exact to="Who-we-are" className="menu__link r-link text-underlined">{t('Data.Header.2')}</NavLink></li>
-                                <div className="align-center">
-                                    <NavLink className="bg-transparent" exact to="/"><img className="fit-picture" src={logo} alt="Logo TLE" /></NavLink>
-                                </div>
                             <li className="menu__group"><NavLink exact to="/Hire" className="menu__link r-link text-underlined">{t('Data.Header.3')}</NavLink></li>
                             <li className="menu__group"><NavLink exact to="/ContactUs" className="menu__link r-link text-underlined">{t('Data.Header.4')}</NavLink></li>
-                            <li className={`menu__group translate_section ${isActive ? 'languages': 'languagesOff'}`}>
+                            <li className="menu__group">
                                 <div className="languageButton ml-4">
                                     <button className='r-link lang-h btn' onClick={() => handleClick('fr')}>
                                         <p className={` ${language === "fr" && "FR"} m-0`}>FR</p>
@@ -54,16 +42,12 @@ function Header() {
                                         <p className={` ${language === "en" && "EN"} m-0`}>EN</p>
                                     </button>
                                 </div>
-                                <button onClick={toggleClass} className=" translate_section_hover d-flex justify-content-center align-items-center text-light border-0">
-                                    <p className={`m-0 ${isActive ? 'languageArrow' : 'null'}`}>▽</p>
-                                </button>
                             </li>
                         </ul>
                     </nav>
                 </div>
-            </header>
-        </main>
-  </div>;
+        </Menu>
+  )
 }
 
-export default Header;
+export default BurgerMenu;
